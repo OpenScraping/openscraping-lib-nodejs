80a0949e899c63a92189476e71deb37b98bce894
zmarty <contact@ovidiudan.com>
2015-12-26 14:36:19 -0800

    Added tonic example

---
+++ example.js
@@ -0,0 +1,17 @@
+// This example file assumes you installed openscraping through npm in a new separate project
+var openscraping = require('./index')
+
+var config = `
+{
+  "title": "//h1",
+  "body": "//div[contains(@class, 'article')]"
+}
+`
+
+var html = '<html><body><h1>Article title</h1><div class="article">Article contents</div></body></html>'
+
+var scrapingResults = openscraping.parse(JSON.parse(config), html)
+
+console.log('Extracted title: ' + scrapingResults.title)
+console.log('Extracted body: ' + scrapingResults.body)
+console.log('Full extracted json: ' + JSON.stringify(scrapingResults))
